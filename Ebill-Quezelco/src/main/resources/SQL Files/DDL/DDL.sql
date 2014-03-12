781fe116f8976a11e9252e69c170588736bc95da
Mae Villanueva <villanueva_marymae@yahoo.com>
2014-03-12 12:24:34 +0800

    tables

--- Ebill-Quezelco/src/main/resources/SQL Files/DDL/DDL.sql
+++ Ebill-Quezelco/src/main/resources/SQL Files/DDL/DDL.sql
@@ -50,7 +50,8 @@ CREATE TABLE tblUserDesignation (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE tblBillConfiguration;
+
+
 CREATE TABLE tblBillConfiguration (
   configName char(12) NOT NULL,
   description varchar(200) NOT NULL,
@@ -67,7 +68,6 @@ CREATE TABLE tblBillConfigurationValue (
   PRIMARY KEY (configName, sequence, configOptionName)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
 DROP TABLE tblBill;
 CREATE TABLE tblBill (
 billId char(12) NOT NULL,
@@ -80,11 +80,12 @@ currentReading decimal NOT NULL,
 previousReading decimal NOT NULL,
 billStatus varchar(20) NOT NULL,
 billPrintDate date NOT NULL,
-latePaymentChargeSw boolean NOT NULL,
 latePaymentChargeDate date,
 currentBillAmount decimal,
+jobId char(12),
 PRIMARY KEY (billId)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
 
 DROP TABLE tblAdjustment;
 CREATE TABLE tblAdjustment (
@@ -98,5 +99,43 @@ adjustmentAmount decimal NOT NULL,
 comments varchar(200),
 PRIMARY KEY (adjustmentId)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
+
+DROP TABLE tblJob;
+CREATE TABLE tblJob (
+jobId bigint(20) AUTO_INCREMENT NOT NULL,
+jobType varchar(20) NOT NULL,
+executedDate TIMESTAMP DEFAULT current_timestamp NOT NULL,
+jobStatus varchar(20) NOT NULL,
+startRunDateTime datetime NOT NULL,
+endRunDateTime datetime,
+PRIMARY KEY (jobId)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
+
+DROP TABLE tblFinancialTransaction;
+CREATE TABLE tblFinancialTransaction (
+ftId char(12) NOT NULL,
+accountNumber varchar(20) NOT NULL,
+ftType varchar(20) NOT NULL,
+billId char(12) NOT NULL,
+payId char(12) NOT NULL,
+dateCreated timestamp default current_timestamp NOT NULL,
+accountDate date,
+PRIMARY KEY (ftId)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
+
+DROP TABLE tblPayment;
+CREATE TABLE tblPayment (
+paymentId char(12) NOT NULL,
+accountNumber char(12) NOT NULL,
+paymentDate timestamp default current_timestamp NOT NULL,
+paymentType varchar(20) NOT NULL,
+amount decimal NOT NULL,
+PRIMARY KEY (paymentId)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
+
 
 COMMIT;
